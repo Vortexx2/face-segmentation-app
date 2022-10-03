4ccba898305df1445032cabb98a1f2fd247956b1
Vortexx2 <janmayshuklay@gmail.com>
2022-10-04 01:11:28 +0530

    Got basic camera functionality working

--- frontend/index.js
+++ frontend/index.js
@@ -2,9 +2,18 @@ const { createApp } = Vue;
 
 const app = createApp({
   data() {
-    return {
-      message: 'FROM VUE',
-    };
+    return {};
+  },
+
+  mounted() {
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(stream => {
+          this.$refs['webcam'].srcObject = stream;
+        })
+        .catch(err => console.error(err));
+    }
   },
 });
 
